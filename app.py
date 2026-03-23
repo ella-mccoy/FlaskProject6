0b958a5841a48c6ac1e5642630c20aa0c845cb62
Ella Mccoy <ellamccoy@Ellas-MacBook-Air.local>
2026-03-23 16:08:28 +0000

    Convert app to recipe finder

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET", "POST"])
+def home():
+    meals = None
+    query = ""
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello from Maggie person 2!'
+    if request.method == "POST":
+        query = request.form.get("meal")
+
+        if query:
+            url = f"https://www.themealdb.com/api/json/v1/1/search.php?s={query}"
+            response = requests.get(url)
+            data = response.json()
+
+            meals = data.get("meals")
+
+    return render_template("index.html", meals=meals, query=query)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
